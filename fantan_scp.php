1b900ca0a006e15c4eaaf8e50a575eda4cb4190b
thanakrit <thanakrit.10197@gmail.com>
2020-06-09 21:33:23 +0700

    now

--- fantan_scp.php
+++ fantan_scp.php
@@ -18,7 +18,7 @@ foreach ($events['events'] as $event) {
 
     if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 
-        $split_slash_count = substr_count($text, "");
+        $split_slash_count = substr_count($text, "\n");
 
         if ($split_slash_count == 0) {
 
