a91c315c6e4306ff9dfdc500fe326572e39a2196
thanakrit <thanakrit.10197@gmail.com>
2020-06-09 19:39:48 +0700

    now

--- fantan_scp.php
+++ fantan_scp.php
@@ -1,4 +1,33 @@
 <?php
+// $url = "http://e-sport.in.th/ssdev/dt/dashboard/auth";
+
+// $data = array(
+//     "username" => "admin",
+//     "password" => "admin",
+// );
+
+// // $request = "";
+
+// // foreach ($data as $key => $val) {
+// //     $request .= $key . "=" . $val . "&";
+// // }
+
+// // $request = rtrim($request, "&");
+
+// $request = json_encode($data);
+
+// $ch = curl_init();
+
+// curl_setopt($ch, CURLOPT_URL, $url);
+// curl_setopt($ch, CURLOPT_POST, 1);
+// curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
+// curl_setopt($ch, CURLOPT_HEADER, 0);
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// $response = curl_exec($ch);
+// curl_close($ch);
+
+// echo $response;
 
 include('./config.php');
 require_once('./custom/fantan_function.php');
