811c266ee82b98185fb4a1cc5acc14f66eefbc56
thanakrit <thanakrit.10197@gmail.com>
2020-06-10 09:01:27 +0700

    now

--- copy.php
+++ copy.php
@@ -78,7 +78,7 @@ foreach ($events['events'] as $event) {
                     ];
                 }
             }
-        } else if ($split_slash_count > 0) {
+        } else {
 
             $reponse_bet = '';
             $bet_type = "multiple";
