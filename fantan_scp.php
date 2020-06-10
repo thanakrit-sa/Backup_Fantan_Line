c96dfb93d34f8835d32118e0e3d3951795f5d0f7
thanakrit <thanakrit.10197@gmail.com>
2020-06-10 09:51:38 +0700

    now

--- fantan_scp.php
+++ fantan_scp.php
@@ -84,7 +84,7 @@ foreach ($events['events'] as $event) {
                         'text' => "รูปแบบการเดิมพันของท่านไม่ถูกต้อง"
                     ];
                 }
-                $reponse_bet = $res_bet;
+                $reponse_bet = $reponse_bet . $res_bet;
                 $i++;
             }
 
