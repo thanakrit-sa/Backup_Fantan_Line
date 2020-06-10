b06f9f40e945f94b107153d195fb7d7fd1dff962
thanakrit <thanakrit.10197@gmail.com>
2020-06-10 10:33:05 +0700

    now

--- fantan_scp.php
+++ fantan_scp.php
@@ -71,7 +71,7 @@ foreach ($events['events'] as $event) {
                     $bet_value = $bet_data[1];
                     if ($bet_text >= 1 && $bet_text <= 4) {
                         // $res_bet = $bet_value . "\r\n";
-                        $res_bet = "#" . $i . " แทง " . $bet_data[0] . " จำนวน " . $bet_data[1] . " บาท ";
+                        $res_bet = "#" . $i . " แทง " . " จำนวน " . $bet_data[1] . " บาท ";
                     } else { 
                         $messages = [
                             'type' => 'text',
