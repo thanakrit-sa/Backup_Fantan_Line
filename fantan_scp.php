5dd2776aba462d6b0941d6c74414e2e9c128ce96
thanakrit <thanakrit.10197@gmail.com>
2020-06-15 19:06:06 +0700

    now

--- fantan_scp.php
+++ fantan_scp.php
@@ -35,8 +35,6 @@ function check_Bet($text)
             } else {
                 $text = "การเดิมพันแบบสเปเชียลสามารถกรอกหมายเลขได้เพียง 1-6 เท่านั้น";
             }
-        } else if (strlen($bet_textSlash) != 3 || strlen($bet_textSlash) != 1) {
-            $text = "การเดิมพันไม่ถูกต้อง";
         } else {
             $text = "แทง/เดิมพันเลข : " . $bet_textSlash . "\r\n" . "จำนวน : " . $bet_valueSlash . " บาท " . "\r\n" . "Code : " . $bet_code;
         }
@@ -51,8 +49,6 @@ function check_Bet($text)
             } else {
                 $text = "การเดิมพันแบบสเปเชียลสามารถกรอกหมายเลขได้เพียง 1-6 เท่านั้น";
             }
-        } else if (strlen($bet_textEqual) != 3 || strlen($bet_textEqual) != 1) {
-            $text = "การเดิมพันไม่ถูกต้อง";
         } else {
             $text = "แทง/เดิมพันเลข : " . $bet_textEqual . "\r\n" . "จำนวน : " . $bet_valueEqual . " บาท " . "\r\n" . "Code : " . $bet_code;
         }
