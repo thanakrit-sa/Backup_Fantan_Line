ed0c70d115b251e8fdfdc97e84ff126ae235efe3
thanakrit <thanakrit.10197@gmail.com>
2020-06-15 16:37:52 +0700

    now

--- fantan_scp.php
+++ fantan_scp.php
@@ -9,6 +9,7 @@ function checkSymbol_Bet($text)
     $bet_slash = explode("/", $text);
     $bet_textSlash = $bet_slash[0];
     $bet_valueSlash = $bet_slash[1];
+    $bet_count = count($bet_textSlash);
 
     #Check Bet_Code
     if ($bet_textEqual == 1 || $bet_textSlash == 1) {
@@ -26,7 +27,7 @@ function checkSymbol_Bet($text)
     #Check Symbol
     if (strpos($text, "/") == true) {
         if ($bet_textSlash >= 1 && $bet_textSlash <= 4) {
-            $text = " แทง/เดิมพันเลข " . $bet_textSlash . " จำนวน " . $bet_valueSlash . " บาท " . "\r\n" . "Code : " . $bet_code;
+            $text = " แทง/เดิมพันเลข " . $bet_textSlash . " จำนวน " . $bet_valueSlash . " บาท " . "\r\n" . "Code : " . $bet_code . $bet_count;
         } else if (count($bet_textSlash) == 3) {
             $text = " แทง/เดิมพันเลข ";
         } else {
