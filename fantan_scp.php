7c3122b480a29e469e3629aa6a16e13aca8d301e
thanakrit <thanakrit.10197@gmail.com>
2020-06-15 15:38:19 +0700

    now

--- fantan_scp.php
+++ fantan_scp.php
@@ -72,7 +72,7 @@ foreach ($events['events'] as $event) {
                 if ($line_id == $userID) {
                     $messages = [
                         'type' => 'text',
-                        'text' => "ชื่อผู้ใช้งาน : " . $user_displayname . "\r\n" . "UserID : " . $userID . "\r\n"
+                        'text' => "ชื่อผู้ใช้งาน : " . $user_displayname . "\r\n" . "UserID : " . $userID . "\r\n" . "ยอดเงินคงเหลือ : " . $credit
                     ];
                 } else {
                     $messages = [
