c6f5d3ccf44e28f5d21b62d3b1a660335197f8c6
thanakrit <thanakrit.10197@gmail.com>
2020-06-22 13:46:00 +0700

    now

--- profile.php
+++ profile.php
@@ -19,7 +19,7 @@
             <div class="form-group m-0 p-0">
                 <div class="input-group mb-2">
                     <div class="input-group-prepend">
-                        <div class="input-group-text"><i class="fas fa-user" style="font-size:30px"></i></div>
+                        <div class="input-group-text" style="width:100px"><i class="fas fa-user" style="font-size:30px"></i></div>
                     </div>
                     <input type="text" class="form-control form-control-lg" readonly placeholder="Spie">
                 </div>
@@ -27,7 +27,7 @@
             <div class="form-group m-0 p-0">
                 <div class="input-group mb-2">
                     <div class="input-group-prepend">
-                        <div class="input-group-text"><i class="far fa-id-card" style="font-size:28px"></i></div>
+                        <div class="input-group-text" style="width:100px"><i class="far fa-id-card" style="font-size:30px"></i></div>
                     </div>
                     <input type="text" class="form-control form-control-lg" placeholder="Spie">
                 </div>
