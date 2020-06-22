ca5f6a28b64c2db7c6ac1be5c4483ffd79597215
thanakrit <thanakrit.10197@gmail.com>
2020-06-22 15:03:16 +0700

    now

--- profile.php
+++ profile.php
@@ -15,7 +15,7 @@
 
 <body style="background-image: url(https://img.freepik.com/free-vector/retro-styled-pattern-background_1048-6593.jpg?size=338&ext=jpg);">
     <?
-        $userID = $_POST['userID'];
+        $userID = $_GET['userID'];
         echo $userID;
         // $ch = curl_init('http://e-sport.in.th/ssdev/fantan/api/user_test/profile/' . $userID);
         // curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
